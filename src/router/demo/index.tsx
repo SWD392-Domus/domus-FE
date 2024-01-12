8eead4e5c6fda58f0e1a4faf64fd905db9769e16
TamDNB <donguyenbaotam546@gmail.com>
2024-01-12 19:22:45 +0700

    demo shadcn

--- src/router/demo/index.tsx
+++ src/router/demo/index.tsx
@@ -3,6 +3,15 @@ import styles from "./Counter.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { counterSelector } from "./slice/selector";
 import { addAmount, decrease, increase } from "./slice";
+import { Button, buttonVariants } from "@/components/Button";
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/Accordion"
+
+
 
 interface Props {
     // define your props here
@@ -57,6 +66,37 @@ const Demo: React.FC<Props> = (props) => {
                     </div>
                 </div>
             </header>
+            <div className="flex justify-center p-20">
+                <div className="w-56">
+
+                <Accordion type="single" collapsible className="w-full">
+                    <AccordionItem value="item-1">
+                        <AccordionTrigger>A tâm đẹp trai ko?</AccordionTrigger>
+                        <AccordionContent>
+                            Yes.
+                        </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="item-2">
+                        <AccordionTrigger>fuck?</AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It comes with default styles that matches the other
+                            components&apos; aesthetic.
+                        </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="item-3">
+                        <AccordionTrigger>hihihih?</AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It's animated by default, but you can disable it if you prefer.
+                        </AccordionContent>
+                    </AccordionItem>
+                </Accordion>
+            </div>
+            </div>
+            
+            <Button variant="destructive">Button</Button>
+
+
+
         </div>
     );
 };
