d3dfb058687c8be8f7de30ee3752157678070e28
Huy Nguyễn Bá <nguyenbahuytk@gmail.com>
2024-01-29 15:17:12 +0700

    [HuyNB] background dark deletion

--- src/components/CustomerComponents/CustomerLayout.tsx
+++ src/components/CustomerComponents/CustomerLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 const CustomerLayout = () => {
     return (
-        <div className="flex flex-col min-h-screen bg-black">
+        <div className="flex flex-col min-h-screen">
             <header className="bg-black bg-cover h-10 text-white pt-2">
                 <div className="container flex flex-row justify-between my-auto">
                     <p className="logo">Domus Logo here</p>
