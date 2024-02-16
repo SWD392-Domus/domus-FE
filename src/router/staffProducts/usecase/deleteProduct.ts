e447250a4d4ce7233cc02acb8a8f53459170de0a
Huy Nguyễn Bá <nguyenbahuytk@gmail.com>
2024-02-17 00:40:27 +0700

    [HuyNB] handle delete product

--- src/router/staffProducts/usecase/deleteProduct.ts
+++ src/router/staffProducts/usecase/deleteProduct.ts
@@ -1,11 +1,6 @@
-import { ToastDestructive } from "../components/Toast";
 import { deleteProductService } from "../service";
 
 export const deleteProduct = async (id: string) => {
   const response = await deleteProductService(id);
-  if (response.status === 200) {
-    return ToastDestructive;
-  } else {
-    return ToastDestructive;
-  }
+  return response.status;
 };
