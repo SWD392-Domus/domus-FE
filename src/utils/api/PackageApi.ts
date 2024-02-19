d82756cad8401f1110f1136886394e5236b73cf0
Huy Nguyễn Bá <nguyenbahuytk@gmail.com>
2024-02-19 13:29:55 +0700

    [HuyNB] sort button ui update

--- src/utils/api/PackageApi.ts
+++ src/utils/api/PackageApi.ts
@@ -11,6 +11,7 @@ export const packageApi = {
 const token =
   "Bearer " +
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImR1eXN0YWZmQGRvbXVzLmNvbSIsInN1YiI6ImM3MTNhYWNjLTM1ODItNDU5OC04NjcwLTIyNTkwZDgzNzE3OSIsIm5hbWUiOiJkdXlzdGFmZkBkb211cy5jb20iLCJyb2xlIjpbIkNsaWVudCIsIlN0YWZmIl0sIm5iZiI6MTcwODE4NzgzMCwiZXhwIjoxNzA4MTg4NzMwLCJpYXQiOjE3MDgxODc4MzAsImlzcyI6Imh0dHA6Ly8yMy4xMDIuMjI2LjExODo0NDMiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjMwMDAifQ.b2S6g3aNMzhI6bma0nl_8Rnk2DgJomRZXrlgFNC5pKs";
+
 export const packageStaffApi = {
   deletePackage: (id: string) => {
     return remove(`/Packages/${id}`, {}, {}, { Authorization: token });
