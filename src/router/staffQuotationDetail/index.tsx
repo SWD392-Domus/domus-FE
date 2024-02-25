64921fcdf1edaea0f363c4ee8b7d23b71a27e21e
Duc bao <baondforwork@gmail.com>
2024-02-25 23:38:54 +0700

    [Baond] reformat logic

--- src/router/staffQuotationDetail/index.tsx
+++ src/router/staffQuotationDetail/index.tsx
@@ -1,16 +1,34 @@
 import { DataTable } from "./components/Table";
-import { columns, data } from './components/Table/column'
-import { userInfo, quotationInfo, negoNum, staffInfo, negoList } from "./constants"
-import { EnvelopeClosedIcon, HomeIcon, PersonIcon, ChatBubbleIcon } from "@radix-ui/react-icons"
-import { Textarea } from "@/components/ui/Textarea"
-import { SendButton, UpdateButton, DeleteButton, MakeContractButton } from "./components/Button";
+import { columns, data } from "./components/Table/column";
+import {
+    userInfo,
+    quotationInfo,
+    negoNum,
+    staffInfo,
+    negoList,
+} from "./constants";
+import {
+    EnvelopeClosedIcon,
+    HomeIcon,
+    PersonIcon,
+    ChatBubbleIcon,
+} from "@radix-ui/react-icons";
+import { Textarea } from "@/components/ui/Textarea";
+import {
+    SendButton,
+    UpdateButton,
+    DeleteButton,
+    MakeContractButton,
+} from "./components/Button";
 
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { getQuotationById } from "./usecase";
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import selector from "./slice/selector";
 import { actions } from "./slice";
+import { Avatar } from "@/components/ui/Avatar";
+import { AvatarImage } from "@radix-ui/react-avatar";
 
 interface Props {
     // define your props here
@@ -28,7 +46,6 @@ const QuotationDetail: React.FC<Props> = () => {
     const products: any[] = useSelector(selector.products);
     const negotiationLog: any = useSelector(selector.negotiationLog);
 
-
     const [updated, setUpdated] = useState(false);
 
     // const location = useLocation();
@@ -41,7 +58,7 @@ const QuotationDetail: React.FC<Props> = () => {
                 console.log(response);
 
                 if (response) {
-                    dispatch(actions.setQuotation(response))
+                    dispatch(actions.setQuotation(response));
                     // console.log(response)
                     dispatch(actions.getQuotationInfo());
                     setUpdated(true);
@@ -53,11 +70,12 @@ const QuotationDetail: React.FC<Props> = () => {
     }
 
     useEffect(() => {
-        fetchData()
+        fetchData();
     }, []);
+
     return (
         <>
-            {updated &&
+            {updated && (
                 <div className="flex flex-row flex-wrap">
                     <div className="left-side-2 basis-1/3">
                         <div className="my-7 text-2xl font-semibold">
@@ -70,8 +88,13 @@ const QuotationDetail: React.FC<Props> = () => {
                         <div className="staff-assigned-info my-7">
                             <div className="mb-2">Assigned Staff</div>
                             <div className="flex flex-row mb-2">
-                                <img src={staff.profileImage} alt="" className="h-10 w-10 bg-gray-300 mr-2" />
-                                <span className="my-auto font-medium">{staff.userName}</span>
+                                <Avatar>
+                                    <AvatarImage src={staff.profileImage} />
+                                </Avatar>
+
+                                <span className="my-auto font-medium">
+                                    {staff.userName}
+                                </span>
                             </div>
                         </div>
                     </div>
@@ -88,41 +111,69 @@ const QuotationDetail: React.FC<Props> = () => {
                                         </div>
                                         <div className="text-xs mb-1 ">
                                             <div className="flex flex-row">
-                                                <div className="font-semibold mr-1">Beginning Date</div>
-                                                <p>{expireAt}</p>
+                                                <div className="font-semibold mr-1">
+                                                    Request Date
+                                                </div>
+                                                <p>
+                                                    {new Date(
+                                                        expireAt
+                                                    ).toDateString()}
+                                                </p>
                                             </div>
                                         </div>
                                         <div className="text-xs">
                                             <div className="flex flex-row">
-                                                <div className="font-semibold mr-1">Ending Date</div>
-                                                <p>{expireAt}</p>
+                                                <div className="font-semibold mr-1">
+                                                    Exprie Date
+                                                </div>
+                                                <p>
+                                                    {" "}
+                                                    {new Date(
+                                                        expireAt
+                                                    ).toDateString()}
+                                                </p>
                                             </div>
                                         </div>
                                     </div>
-                                    <div className="right-side-3 basis-2/3">
+                                    <div className="right-side-3 basis-2/3 font-sans">
                                         <div className="text-sm font-semibold mr-5 mb-1 pb-1 border-solid border-b-2 border-zinc-400">
                                             Customer Information
                                         </div>
-                                        <div className="ava-header flex flex-row mb-1">
-                                            <img src={customer.profileImage} alt="" className="h-10 w-10 bg-gray-300 mr-2" />
-                                            <span className="my-auto font-medium">{customer.userName}</span>
+                                        <div className=" flex flex-row mb-1">
+                                            <Avatar className="mr-2">
+                                                <AvatarImage
+                                                    className="w-full"
+                                                    src={customer.profileImage}
+                                                />
+                                            </Avatar>
+                                            <span className="my-auto font-medium font-sans">
+                                                {customer.fullName}
+                                            </span>
                                         </div>
                                         <div className="user-info-body text-sm">
                                             <div className="mail-info flex flex-row mb-1">
                                                 <HomeIcon className="my-auto mr-2" />
-                                                <span className="">{customer.address}</span>
+                                                <span className="font-sans">
+                                                    {customer.address
+                                                        ? customer.address
+                                                        : "N/A"}
+                                                </span>
                                             </div>
                                         </div>
                                         <div className="user-info-body text-sm">
                                             <div className="mail-info flex flex-row mb-1">
                                                 <EnvelopeClosedIcon className="my-auto mr-2" />
-                                                <span className="">{customer.email}</span>
+                                                <span className="">
+                                                    {customer.email}
+                                                </span>
                                             </div>
                                         </div>
                                         <div className="user-info-body text-sm">
                                             <div className="phone-info flex flex-row mb-1">
                                                 <PersonIcon className="my-auto mr-2" />
-                                                <span className="">{customer.phoneNumber}</span>
+                                                <span className="">
+                                                    {customer.phoneNumber}
+                                                </span>
                                             </div>
                                         </div>
                                     </div>
@@ -133,9 +184,14 @@ const QuotationDetail: React.FC<Props> = () => {
                                     Details
                                 </div>
                                 <div className="mx-auto py-5">
-                                    <DataTable columns={columns} data={products} />
+                                    <DataTable
+                                        columns={columns}
+                                        data={products}
+                                    />
                                     <div className="p-2 flex flex-row justify-between font-semibold border-b-2 border-zinc-100">
-                                        <div className="total-price-title mx-11">Total Price</div>
+                                        <div className="total-price-title mx-11">
+                                            Total Price
+                                        </div>
                                         <div className="total-price-amount">
                                             {new Intl.NumberFormat("en-US", {
                                                 style: "currency",
@@ -150,7 +206,6 @@ const QuotationDetail: React.FC<Props> = () => {
                                 <DeleteButton></DeleteButton>
                                 <MakeContractButton></MakeContractButton>
                             </div>
-
                         </div>
                         <div className="nego-info">
                             <div className="my-3 text-2xl font-semibold">
@@ -159,57 +214,79 @@ const QuotationDetail: React.FC<Props> = () => {
                             <div className="nego-info-body text-sm">
                                 <div className="mail-info flex flex-row mb-1">
                                     <ChatBubbleIcon className="my-auto mr-2" />
-                                    <span className="">{negotiationLog?.messages?.length} negotiation chats</span>
+                                    <span className="">
+                                        {negotiationLog?.messages?.length}{" "}
+                                        negotiation chats
+                                    </span>
                                 </div>
                             </div>
                             <div className="my-5">
                                 <div className="flex flex-row mb-2">
-                                    <img src={staff.profileImage} alt="" className="h-10 w-10 bg-gray-300 mr-2" />
-                                    <Textarea>
-                                    </Textarea>
+                                    <img
+                                        src={staff.profileImage}
+                                        alt=""
+                                        className="h-10 w-10 bg-gray-300 mr-2"
+                                    />
+                                    <Textarea></Textarea>
                                 </div>
                                 <div className="flex justify-end">
                                     <SendButton></SendButton>
                                 </div>
                             </div>
 
-                            {negotiationLog?.messages?.map((nego) => (
-                                nego.isCustomerMessage ?
+                            {negotiationLog?.messages?.map((nego: any) =>
+                                nego.isCustomerMessage ? (
                                     <div className="flex flex-row my-5">
-
-                                        <img src={customer.profileImage} alt="" className="h-10 w-10 bg-gray-300 mr-2" />
+                                        <img
+                                            src={customer.profileImage}
+                                            alt=""
+                                            className="h-10 w-10 bg-gray-300 mr-2"
+                                        />
 
                                         <div className="nego-detail">
+                                            <span className="my-auto font-medium">
+                                                {customer.userName}
+                                            </span>
 
-                                            <span className="my-auto font-medium">{customer.userName}</span>
+                                            <div className="date-info text-xs">
+                                                Published on {nego.sentAt}
+                                            </div>
 
-                                            <div className="date-info text-xs">Published on {nego.sentAt}</div>
-
-                                            <div className="cmt-info text-sm">{nego.content}
+                                            <div className="cmt-info text-sm">
+                                                {nego.content}
                                             </div>
                                         </div>
                                     </div>
-                                    : <div className="flex flex-row my-5">
-
-                                        <img src={staff.profileImage} alt="" className="h-10 w-10 bg-gray-300 mr-2" />
+                                ) : (
+                                    <div className="flex flex-row my-5">
+                                        <img
+                                            src={staff.profileImage}
+                                            alt=""
+                                            className="h-10 w-10 bg-gray-300 mr-2"
+                                        />
 
                                         <div className="nego-detail">
+                                            <span className="my-auto font-medium">
+                                                {staff.userName}
+                                            </span>
 
-                                            <span className="my-auto font-medium">{staff.userName}</span>
+                                            <div className="date-info text-xs">
+                                                Published on {nego.sentAt}
+                                            </div>
 
-                                            <div className="date-info text-xs">Published on {nego.sentAt}</div>
-
-                                            <div className="cmt-info text-sm">{nego.content}
+                                            <div className="cmt-info text-sm">
+                                                {nego.content}
                                             </div>
                                         </div>
                                     </div>
-                            ))}
-                        </div >
-                    </div >
-                </div >
-            }
+                                )
+                            )}
+                        </div>
+                    </div>
+                </div>
+            )}
         </>
-    )
-}
+    );
+};
 
-export default QuotationDetail
+export default QuotationDetail;
--- src/router/staffQuotationDetail/slice/index.ts
+++ src/router/staffQuotationDetail/slice/index.ts
@@ -3,107 +3,107 @@ import generateActions from "./generateActions";
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 export const initialState = {
-  id: "",
-  staff: {},
-  customer: {},
-  status: "",
-  totalPrice: 0,
-  expireAt: "",
-  products: [],
-  negotiationLog: {},
+    id: "",
+    staff: {},
+    customer: {},
+    status: "",
+    totalPrice: 0,
+    expireAt: "",
+    products: [],
+    negotiationLog: {},
 };
 
 export const name = "viewQuotation";
 
 const slice = createSlice({
-  name,
-  initialState,
-  reducers: {
-    ...generateActions(initialState),
+    name,
+    initialState,
+    reducers: {
+        ...generateActions(initialState),
 
-    modal_confirm: (state: any, action: any) => {
-      state.modal.confirm = action.payload;
-    },
+        modal_confirm: (state: any, action: any) => {
+            state.modal.confirm = action.payload;
+        },
 
-    setQuotation: (state: any, action: any) => {
-      state.id = action.payload.id;
-      state.customer = action.payload.customer;
-      state.staff = action.payload.staff;
-      state.status = action.payload.status;
-      state.totalPrice = action.payload.totalPrice;
-      state.expireAt = action.payload.expireAt;
-      state.products = action.payload.products;
-      state.negotiationLog = action.payload.negotiationLog;
-    },
-    getQuotationInfo: (state: any) => {
-      state.info = {
-        id: state.id,
-        customer: state.customer,
-        staff: state.staff,
-        status: state.status,
-        totalPrice: state.totalPrice,
-        expireAt: state.expireAt,
-        products: state.products,
-        negotiationLog: state.negotiationLog,
-      };
-    },
+        setQuotation: (state: any, action: any) => {
+            state.id = action.payload.id;
+            state.customer = action.payload.customer;
+            state.staff = action.payload.staff;
+            state.status = action.payload.status;
+            state.totalPrice = action.payload.totalPrice;
+            state.expireAt = action.payload.expireAt;
+            state.products = action.payload.products;
+            state.negotiationLog = action.payload.negotiationLog;
+        },
+        getQuotationInfo: (state: any) => {
+            state.info = {
+                id: state.id,
+                customer: state.customer,
+                staff: state.staff,
+                status: state.status,
+                totalPrice: state.totalPrice,
+                expireAt: state.expireAt,
+                products: state.products,
+                negotiationLog: state.negotiationLog,
+            };
+        },
 
-    addProduct: (state: any, action: any | null) => {
-      state.productDetails.push({
-        ...action.payload.details[0],
-        productName: action.payload.productName,
-      });
-    },
-    addService: (state: any, action: any | null) => {
-      state.services.push(action.payload);
-    },
-    deleteProduct: (state: any, action: PayloadAction<string>) => {
-      const productIdToDelete = action.payload;
-      const indexToDelete = state.productDetails.findIndex(
-        (product: any) => product.id === productIdToDelete
-      );
+        addProduct: (state: any, action: any | null) => {
+            state.productDetails.push({
+                ...action.payload.details[0],
+                productName: action.payload.productName,
+            });
+        },
+        addService: (state: any, action: any | null) => {
+            state.services.push(action.payload);
+        },
+        deleteProduct: (state: any, action: PayloadAction<string>) => {
+            const productIdToDelete = action.payload;
+            const indexToDelete = state.productDetails.findIndex(
+                (product: any) => product.id === productIdToDelete
+            );
 
-      if (indexToDelete !== -1) {
-        state.productDetails.splice(indexToDelete, 1);
-      }
+            if (indexToDelete !== -1) {
+                state.productDetails.splice(indexToDelete, 1);
+            }
+        },
+        deleteSerivce: (state: any, action: PayloadAction<string>) => {
+            const productIdToDelete = action.payload;
+            const indexToDelete = state.services.findIndex(
+                (product: any) => product.id === productIdToDelete
+            );
+
+            if (indexToDelete !== -1) {
+                state.services.splice(indexToDelete, 1);
+            }
+        },
+        deleteManyService: (state: any, action: PayloadAction<string[]>) => {
+            const idsToDelete = action.payload;
+
+            idsToDelete.forEach((id: string) => {
+                const indexToDelete = state.services.findIndex(
+                    (product: any) => product.id === id
+                );
+
+                if (indexToDelete !== -1) {
+                    state.services.splice(indexToDelete, 1);
+                }
+            });
+        },
+        deleteManyProducts: (state: any, action: PayloadAction<string[]>) => {
+            const idsToDelete = action.payload;
+
+            idsToDelete.forEach((id: string) => {
+                const indexToDelete = state.productDetails.findIndex(
+                    (product: any) => product.id === id
+                );
+
+                if (indexToDelete !== -1) {
+                    state.productDetails.splice(indexToDelete, 1);
+                }
+            });
+        },
     },
-    deleteSerivce: (state: any, action: PayloadAction<string>) => {
-      const productIdToDelete = action.payload;
-      const indexToDelete = state.services.findIndex(
-        (product: any) => product.id === productIdToDelete
-      );
-
-      if (indexToDelete !== -1) {
-        state.services.splice(indexToDelete, 1);
-      }
-    },
-    deleteManyService: (state: any, action: PayloadAction<string[]>) => {
-      const idsToDelete = action.payload;
-
-      idsToDelete.forEach((id: string) => {
-        const indexToDelete = state.services.findIndex(
-          (product: any) => product.id === id
-        );
-
-        if (indexToDelete !== -1) {
-          state.services.splice(indexToDelete, 1);
-        }
-      });
-    },
-    deleteManyProducts: (state: any, action: PayloadAction<string[]>) => {
-      const idsToDelete = action.payload;
-
-      idsToDelete.forEach((id: string) => {
-        const indexToDelete = state.productDetails.findIndex(
-          (product: any) => product.id === id
-        );
-
-        if (indexToDelete !== -1) {
-          state.productDetails.splice(indexToDelete, 1);
-        }
-      });
-    },
-  },
 });
 
 injectReducer(name, slice.reducer);
