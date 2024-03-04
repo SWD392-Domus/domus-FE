8f6efdc558d57cf156ded3546d09e5ef5dff5c18
Huy Nguyễn Bá <nguyenbahuytk@gmail.com>
2024-03-04 11:47:21 +0700

    [HuyNB] package customization done

--- src/router/packageDetails/components/Button/RequestQuotationButton/ConfirmButton.tsx
+++ src/router/packageDetails/components/Button/RequestQuotationButton/ConfirmButton.tsx
@@ -5,12 +5,16 @@ import { ToastAction } from "@/components/ui/Toast/toast";
 import { useToast } from "@/components/ui/Toast/use-toast";
 import { createQuotation } from "@/router/customerCart/usecase/createQuotation.ts";
 import { useNavigate } from "react-router-dom";
+import selector from "@/router/packageDetails/slice/selector";
+import { useSelector } from "react-redux";
 
 interface Props {
     id: string;
 }
 
 export const ConfirmButton: React.FC<Props> = (props) => {
+    const packageA: any = useSelector(selector.packageA);
+
     const { toast } = useToast();
     const navigate = useNavigate();
     const handleClick = async () => {
@@ -18,6 +22,19 @@ export const ConfirmButton: React.FC<Props> = (props) => {
             const res = await createQuotation({
                 // expireAt: "2024-09-24T06:54:12.762Z",
                 packageId: props.id,
+                services: packageA.services.map((ser: any) => {
+                    return {
+                        serviceId: ser.id,
+                        price: ser.price,
+                    }
+                }),
+                productDetails: packageA.productDetails.map((productDetail: any) => {
+                    return {
+                        id: productDetail.id,
+                        quantity: productDetail.quantity,
+                        price: productDetail.displayPrice,
+                    }
+                })
             });
             if (res === 200) {
                 toast({
@@ -28,7 +45,7 @@ export const ConfirmButton: React.FC<Props> = (props) => {
                 });
                 localStorage.removeItem("cart");
                 setTimeout(() => {
-                    window.location.reload();
+                    navigate("/customer/settings/quotations");
                 }, 2000);
             } else {
                 toast({
