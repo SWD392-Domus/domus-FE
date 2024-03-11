4403ff35b9297e48419d69cdd872cdc69b8be5e9
Huy Nguyễn Bá <nguyenbahuytk@gmail.com>
2024-03-12 01:52:54 +0700

    [HuyNB] contract list api

--- src/utils/api/contractApi.ts
+++ src/utils/api/contractApi.ts
@@ -1,32 +1,47 @@
 import { get, post } from "./ApiCaller";
 
+export const contractStaffApi = {
+  getContractsPaging: (
+    searchField: string,
+    searchValue: string,
+    sortField: string,
+    descending: boolean,
+    pageSize: number,
+    pageIndex: number,
+    token: string
+  ) => {
+    return get(
+      `/Contract/search?SearchField=${searchField}&SearchValue=${searchValue}&SortField=${sortField}&Descending=${descending}&PageSize=${pageSize}&PageIndex=${pageIndex}`,
+      {},
+      {
+        Authorization: token,
+      }
+    );
+  },
+};
+
 export const contractApi = {
-    getContractDetail: (id: string, token: string) => {
-        return get(`/Contract/${id}`, {}, { Authorization: `Bearer ${token}` });
-    },
-    createContract: (data: any, token: string) => {
-        return post(
-            "/Contract",
-            data,
-            {},
-            { Authorization: `Bearer ${token}` }
-        );
-    },
-    getContractPaging: (
-        searchField: string,
-        searchValue: string,
-        sortField: string,
-        descending: boolean,
-        pageSize: number,
-        pageIndex: number,
-        token: string
-    ) => {
-        return get(
-            `/Contract/search?SearchField=${searchField}&SearchValue=${searchValue}&SortField=${sortField}&Descending=${descending}&PageSize=${pageSize}&PageIndex=${pageIndex}`,
-            {},
-            {
-                Authorization: token,
-            }
-        );
-    },
+  getContractDetail: (id: string, token: string) => {
+    return get(`/Contract/${id}`, {}, { Authorization: `Bearer ${token}` });
+  },
+  createContract: (data: any, token: string) => {
+    return post("/Contract", data, {}, { Authorization: `Bearer ${token}` });
+  },
+  getContractPaging: (
+    searchField: string,
+    searchValue: string,
+    sortField: string,
+    descending: boolean,
+    pageSize: number,
+    pageIndex: number,
+    token: string
+  ) => {
+    return get(
+      `/Contract/search?SearchField=${searchField}&SearchValue=${searchValue}&SortField=${sortField}&Descending=${descending}&PageSize=${pageSize}&PageIndex=${pageIndex}`,
+      {},
+      {
+        Authorization: token,
+      }
+    );
+  },
 };
