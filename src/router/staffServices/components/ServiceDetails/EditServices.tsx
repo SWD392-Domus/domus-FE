7de62ceb3f98d60804cb0874a13af1762e83bb04
Huy Nguyễn Bá <nguyenbahuytk@gmail.com>
2024-03-17 19:36:38 +0700

    [HuyNB] update services edit

--- src/router/staffServices/components/ServiceDetails/EditServices.tsx
+++ src/router/staffServices/components/ServiceDetails/EditServices.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { getServiceByIdService } from "../../service";
 import { toast } from "@/components/ui/Toast/use-toast";
 import {
+  DialogContent,
   DialogDescription,
   DialogHeader,
   DialogTitle,
@@ -21,6 +22,9 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/ui/Button/Button";
 import { editServiceService } from "../../service/editService";
+import selector from "./slice/selector"
+import { actions } from "./slice"
+import { useDispatch, useSelector } from "react-redux";
 
 type Props = {
   id: string;
@@ -31,7 +35,9 @@ type Props = {
 //   price: number;
 // };
 const EditServices: React.FC<Props> = ({ id }) => {
-  // const [service, setService] = useState<ServiceProps>();
+  const dispatch = useDispatch();
+  const name: string = useSelector(selector.name);
+  const price: number = useSelector(selector.price);
   const [updated, setUpdated] = useState(false);
   async function getServiceDetails() {
     try {
@@ -39,6 +45,7 @@ const EditServices: React.FC<Props> = ({ id }) => {
       const response = res.data;
       // console.log("response", response);
       if (response.isSuccess) {
+        dispatch(actions.setService(response.data));
         form.setValue("name", response.data.name);
         form.setValue("price", response.data.rproce);
         setUpdated(true);
@@ -54,12 +61,7 @@ const EditServices: React.FC<Props> = ({ id }) => {
 
   useEffect(() => {
     getServiceDetails();
-  }, []);
-
-  // const formatted = new Intl.NumberFormat("en-US", {
-  //   style: "currency",
-  //   currency: "VND",
-  // }).format(service?.price || 0);
+  });
 
   const formSchema = z.object({
     name: z.string().nonempty({ message: "Name is required" }),
@@ -68,10 +70,10 @@ const EditServices: React.FC<Props> = ({ id }) => {
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    // defaultValues: {
-    //   name: service?.name,
-    //   price: service?.price,
-    // },
+    defaultValues: {
+      name: name,
+      price: price,
+    },
   });
 
   const onSubmit = async (data: z.infer<typeof formSchema>) => {
@@ -98,58 +100,58 @@ const EditServices: React.FC<Props> = ({ id }) => {
       });
     }
   };
+
   return (
-    <>
+    <DialogContent autoSave="false">
       <DialogHeader>
         <DialogTitle>Edit Service</DialogTitle>
         <DialogDescription>
           Make changes to your Service here. Click save when you're done.
         </DialogDescription>
-      </DialogHeader>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          {updated && (
-            <>
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Service Name</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormDescription>
-                      This is your public display service name.
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="price"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Price</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormDescription>
-                      This is your public display service price.
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <div className="flex justify-end">
-                <Button type="submit">Save</Button>
-              </div>
-            </>
-          )}
-        </form>
-      </Form>
-    </>
+      </DialogHeader >
+
+      {updated && (
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Service Name</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display service name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="price"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Price</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display service price.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex justify-end">
+              <Button type="submit">Save</Button>
+            </div>
+          </form>
+        </Form>
+      )}
+    </DialogContent >
   );
 };
 
