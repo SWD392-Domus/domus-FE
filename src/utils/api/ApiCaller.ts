46b84ba67b0b66a52e08bc296bea1fe198cffb0a
Huy Nguyễn Bá <nguyenbahuytk@gmail.com>
2024-03-24 18:27:44 +0700

    [HuyNB] localhost

--- src/utils/api/ApiCaller.ts
+++ src/utils/api/ApiCaller.ts
@@ -2,64 +2,64 @@
 
 import axios, { AxiosResponse } from "axios";
 
-const defaultURL = "https://domus.io.vn/api";
+const defaultURL = "https://localhost:10000/api";
 
 interface PublicCallParams {
-    method: string;
-    url: string;
-    headers?: Record<string, string>;
-    params?: Record<string, any>;
-    data?: any;
+  method: string;
+  url: string;
+  headers?: Record<string, string>;
+  params?: Record<string, any>;
+  data?: any;
 }
 
 const PublicCall = (
-    method: string,
-    endpoint: string,
-    headers?: Record<string, string>,
-    params?: Record<string, any>,
-    body?: any
+  method: string,
+  endpoint: string,
+  headers?: Record<string, string>,
+  params?: Record<string, any>,
+  body?: any
 ) => {
-    const config: PublicCallParams = {
-        method,
-        url: defaultURL + endpoint,
-        headers: { ...headers },
-        params: { ...params },
-        data: body,
-    };
-    return axios(config);
+  const config: PublicCallParams = {
+    method,
+    url: defaultURL + endpoint,
+    headers: { ...headers },
+    params: { ...params },
+    data: body,
+  };
+  return axios(config);
 };
 
 export const get = (
-    endpoint: string,
-    params?: Record<string, any>,
-    headers?: Record<string, string>
+  endpoint: string,
+  params?: Record<string, any>,
+  headers?: Record<string, string>
 ): Promise<AxiosResponse> => {
-    return PublicCall("GET", endpoint, headers, params);
+  return PublicCall("GET", endpoint, headers, params);
 };
 
 export const post = (
-    endpoint: string,
-    body?: any,
-    params?: Record<string, any>,
-    headers?: Record<string, string>
+  endpoint: string,
+  body?: any,
+  params?: Record<string, any>,
+  headers?: Record<string, string>
 ): Promise<AxiosResponse> => {
-    return PublicCall("POST", endpoint, headers, params, body);
+  return PublicCall("POST", endpoint, headers, params, body);
 };
 
 export const put = (
-    endpoint: string,
-    body?: any,
-    params?: Record<string, any>,
-    headers?: Record<string, string>
+  endpoint: string,
+  body?: any,
+  params?: Record<string, any>,
+  headers?: Record<string, string>
 ): Promise<AxiosResponse> => {
-    return PublicCall("PUT", endpoint, headers, params, body);
+  return PublicCall("PUT", endpoint, headers, params, body);
 };
 
 export const remove = (
-    endpoint: string,
-    body?: any,
-    params?: Record<string, any>,
-    headers?: Record<string, string>
+  endpoint: string,
+  body?: any,
+  params?: Record<string, any>,
+  headers?: Record<string, string>
 ): Promise<AxiosResponse> => {
-    return PublicCall("DELETE", endpoint, headers, params, body);
+  return PublicCall("DELETE", endpoint, headers, params, body);
 };
